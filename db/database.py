3aa7c2d92e49c9e476f3fca82b4f999330c5adb1
DrCraZZy <maxim.bienemann@gmail.com>
2022-01-12 21:37:03 +0100

    delete some unnecessary code

--- db/database.py
+++ db/database.py
@@ -1,14 +1,10 @@
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from core.config import DATABASE_NAME
 
-DATABASE_NAME = 'TheTransporter.sqlite'
 
-engine = create_engine(f'sqlite:///{DATABASE_NAME}')
+engine = create_engine(DATABASE_NAME)
 metadata = MetaData()
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
 
 
 def create_db():
-    Base.metadata.create_all(engine)
+    metadata.create_all(bind=engine)
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from db.customer import customer_data, customer_contract, customer_contact, customer_order
+from db.references import physical_properties
+from db.transporter import transporter_data, transporter_contact, transporter_vehicles, transporter_contracts
+
+from db.database import create_db
+
+def main():
+    create_db()
+
 if __name__ == '__main__':
-    ...
+    main()
