74664c614610460d684be3ec06642cb8d0ba262b
DrCraZZy <maxim.bienemann@gmail.com>
2022-01-13 21:43:37 +0100

    Start with endpoints progs

--- db/database.py
+++ db/database.py
@@ -1,9 +1,10 @@
+from databases import Database
 from sqlalchemy import create_engine, MetaData
 from core.config import DATABASE_NAME
 
-
 engine = create_engine(DATABASE_NAME)
 metadata = MetaData()
+database = Database()
 
 
 def create_db():
--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+import uvicorn
+from fastapi import FastAPI
+
 from db.customer import customer_data, customer_contract, customer_contact, customer_order
 from db.references import physical_properties
 from db.transporter import transporter_data, transporter_contact, transporter_vehicles, transporter_contracts
 
-from db.database import create_db
+from db.database import create_db, database
 
 
 def main():
     create_db()
 
 
+app = FastAPI(title="Employment exchange")
+
+
+# app.include_router(users.router, prefix="/users", tags=["users"])
+# app.include_router(auth.router, prefix="/auth", tags=["auth"])
+# app.include_router(jobs.router, prefix="/jobs", tags=["jobs"])
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
 if __name__ == '__main__':
     main()
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
