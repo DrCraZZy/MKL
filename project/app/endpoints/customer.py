56d6c92a61de615e46ad008e5a5c125bddbe1d70
Maxim <maxim.bienemann@gmail.com>
2022-01-18 15:30:58 +0100

    New CustomerSchema

--- project/app/endpoints/customer.py
+++ project/app/endpoints/customer.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends
 
 from .depends import get_customer_repository
-from project.app.models.customer import CustomerIn, CustomerOut
+from project.app.schema.customer import CustomerSchema
 from project.app.repositories.customers import CustomerRepository
 
 router = APIRouter()
 
 
-@router.post("/", response_model=CustomerOut)
+@router.post("/", response_model=CustomerSchema, status_code=status.HTTP_201_CREATED)
 async def create_customer(
         customer: CustomerIn,
         customers: CustomerRepository = Depends(get_customer_repository)):
--- project/app/schema/customer.py
+++ project/app/schema/customer.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel, EmailStr
-from datetime import datetime
+from datetime import datetime, date
 
 
 # Base customer information
-class CustomerBase(BaseModel):
+class CustomerSchema(BaseModel):
     inn_kpp: str
     ogrn: str
     name: str
-    date_of_formation: datetime | None
+    date_of_formation: date | None
     director: str | None
     legal_address: str
     address: str
@@ -15,14 +15,5 @@ class CustomerBase(BaseModel):
     telephone: str | None
     payment_account: str
     corporate_account: str
-
-
-# Customer gives data for insert it to the database
-class CustomerIn(CustomerBase):
     created_at: datetime = datetime.utcnow()
     updated_at: datetime = datetime.utcnow()
-
-
-# Output customer parameters for viewing
-class CustomerOut(CustomerBase):
-    ...
