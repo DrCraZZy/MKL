a246acfd2f44074b074eaf56cf254d166db5f731
Maxim <maxim.bienemann@gmail.com>
2022-01-18 15:32:08 +0100

    Further endpoints and repositories for customer

--- project/app/endpoints/customer.py
+++ project/app/endpoints/customer.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from typing import List
+from fastapi import APIRouter, Depends, status
 
 from .depends import get_customer_repository
 from project.app.schema.customer import CustomerSchema
@@ -9,6 +10,37 @@ router = APIRouter()
 
 @router.post("/", response_model=CustomerSchema, status_code=status.HTTP_201_CREATED)
 async def create_customer(
-        customer: CustomerIn,
+        customer: CustomerSchema,
         customers: CustomerRepository = Depends(get_customer_repository)):
-    return await customers.create_customer(c=customer)
+    return await customers.create_customer(customer)
+
+
+@router.get("/", response_model=List[CustomerSchema], status_code=status.HTTP_200_OK)
+async def get_customers(
+        limit: int = 100,
+        skip: int = 0,
+        customers: CustomerRepository = Depends(get_customer_repository)):
+    return await customers.get_all_customers(limit=limit, skip=skip)
+
+
+@router.get("/{customer_inn}", response_model=CustomerSchema, status_code=status.HTTP_200_OK)
+async def get_customer_by_inn(
+        customer_inn: str,
+        customers: CustomerRepository = Depends(get_customer_repository)):
+    return await customers.get_customer_by_inn(customer_inn=customer_inn)
+
+
+@router.get("/{customer_email}", response_model=CustomerSchema, status_code=status.HTTP_200_OK)
+async def get_customer_by_email(
+        customer_email: str,
+        customers: CustomerRepository = Depends(get_customer_repository)):
+    return await customers.get_customer_by_email(customer_email=customer_email)
+
+
+@router.put("/{customer_inn}", response_model=CustomerSchema, status_code=status.HTTP_202_ACCEPTED)
+async def update_customer_by_inn(
+        customer_inn: str,
+        customer: CustomerSchema,
+        customers: CustomerRepository = Depends(get_customer_repository)):
+    return await customers.update_customer_by_inn(customer_inn=customer_inn, customer=customer)
+
--- project/app/repositories/customers.py
+++ project/app/repositories/customers.py
@@ -1,3 +1,7 @@
+from typing import List
+from fastapi import HTTPException, status
+from datetime import datetime
+
 from project.app.repositories.base_repository import BaseRepository
 from project.app.schema.customer import CustomerSchema
 from project.app.db.tables.customer import customer_data
@@ -7,7 +11,75 @@ class CustomerRepository(BaseRepository):
 
     async def create_customer(self, customer: CustomerSchema) -> CustomerSchema:
         values = {**customer.dict()}
-        query = customer_contact.insert().values(**values)
+        query = customer_data.insert().values(**values)
         await self.database.execute(query)
 
+        # check for data insertion, is data with new inn_kpp in the table
+        query = customer_data.select().where(customer_data.c.inn_kpp == customer.inn_kpp)
+        data_set = await self.database.execute(query)
+
+        if not data_set:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Customer can't be inserted. Please try is again."
+            )
+
         return customer
+
+    async def get_all_customers(self, limit: int = 100, skip: int = 0) -> List[CustomerSchema]:
+        query = customer_data.select().limit(limit).offset(skip)
+        customer_list = await self.database.fetch_all(query)
+        customer_list_object = []
+        for c in customer_list:
+            customer_list_object.append(CustomerSchema.parse_obj(c))
+        return customer_list_object
+
+    async def get_customer_by_inn(self, customer_inn: str) -> CustomerSchema | None:
+        query = customer_data.select().where(customer_data.c.inn_kpp == customer_inn).first()
+        customer = await self.database.fetch_one(query=query)
+
+        if customer:
+            customer_obj = CustomerSchema.parse_obj(customer)
+            return customer_obj
+
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Customer with inn:'{customer_inn}' does not exists."
+        )
+
+    async def get_customer_by_email(self, customer_email: str) -> CustomerSchema | None:
+        query = customer_data.select().where(customer_data.c.email == customer_email).first()
+        customer = await self.database.fetch_one(query=query)
+
+        if customer:
+            customer_obj = CustomerSchema.parse_obj(customer)
+            return customer_obj
+
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Customer with email:'{customer_email}' does not exists."
+        )
+
+    async def update_customer_by_inn(self, customer_inn: str, customer: CustomerSchema) -> CustomerSchema:
+        updated_customer = CustomerSchema(
+            inn_kpp=customer_inn,
+            ogrn=customer.ogrn,
+            name=customer.name,
+            date_of_formation=customer.date_of_formation,
+            director=customer.director,
+            legal_address=customer.legal_address,
+            address=customer.address,
+            email=customer.email,
+            telephone=customer.telephone,
+            payment_account=customer.payment_account,
+            corporate_account=customer.corporate_account,
+            updated_at=datetime.utcnow()
+        )
+
+        values = {**updated_customer.dict()}
+        values.pop("created_at")
+        values.pop("inn_kpp")
+        query = customer_data.update().where(customer_data.c.inn_kpp == customer_inn).values(**values)
+        await self.database.execute(query)
+
+        return updated_customer
