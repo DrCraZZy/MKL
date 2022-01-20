4658006230b145caa6853d95c96af5fc7e61d1db
Maxim <maxim.bienemann@gmail.com>
2022-01-20 10:12:18 +0100

    Set log-size to 2MB

--- project/app/helper/log.py
+++ project/app/helper/log.py
@@ -2,7 +2,7 @@ from loguru import logger
 
 logger.add(
     "C:\\Workspace\\MKL\\MKL\\project\\log\\logging.log",
-    rotation='00:00',
+    rotation='2MB',
     compression="zip",
     format="{time:YYYY-MM-DD-HH:mm:ss} || {level} || {message}",
     level="INFO"
