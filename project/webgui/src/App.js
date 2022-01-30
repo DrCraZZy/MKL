19a32d5946bda773dec2e254ec3d997fa59140a4
Maxim <maxim.bienemann@gmail.com>
2022-01-30 15:34:05 +0100

    Add gui init

--- project/webgui/src/App.js
+++ project/webgui/src/App.js
@@ -1,9 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+
+import Hearder from "./layout/Header";
+import Footer from "./layout/Footer";
+
+import Registrations from "./pages/registration";
+
+import Home from "./pages/Home";
+import CustomerRegistration from "./pages/CustomerRegistration";
+import TransporterRegeistration from "./pages/TransporterRegeistration";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <>
+            <Hearder />
+            <main className="container content">
+                <Routes>
+                    <Route exact path="/" element={ <Home /> }/>
+                    <Route path="/registration" element={ <Registrations /> }/>
+                    <Route path="/customerregistration" element={ <CustomerRegistration />} />
+                    <Route path="/transporterregeistration" element={ <TransporterRegeistration />} />
+                    <Route element={ <NotFound /> }/>
+                </Routes>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- project/webgui/src/index.js
+++ project/webgui/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+
+import './css/index.css';
+import './css/preloader.css';
+import './css/registrations.css';
+
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
