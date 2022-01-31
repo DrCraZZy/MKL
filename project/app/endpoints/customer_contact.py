7007c63351e9d73769c99426a6b704e5ad92bc52
Maxim <maxim.bienemann@gmail.com>
2022-01-31 06:45:22 +0100

    Change to EndpointAnswer

--- project/app/endpoints/customer_contact.py
+++ project/app/endpoints/customer_contact.py
@@ -1,38 +1,72 @@
-from typing import List
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException, status
 
 from .depends import get_customer_contact_repository
 from project.app.schema.customer_contact import CustomerContactInSchema, CustomerContactOutSchema
 from project.app.repositories.customer_contact import CustomerContactRepository
+from project.app.helper.endpoint_answer import EndpointAnswer
+from project.app.helper.message_parser import parse_message
 
 router = APIRouter()
 
 
-@router.get("/{customer_inn}", response_model=List[CustomerContactOutSchema], status_code=status.HTTP_200_OK)
+@router.get("/{customer_inn}", response_model=EndpointAnswer, status_code=status.HTTP_200_OK)
 async def get_customer_contacts(
         customer_inn: str,
         contacts: CustomerContactRepository = Depends(get_customer_contact_repository)):
-    return await contacts.get_customer_contacts_by_inn(customer_inn=customer_inn)
+    answer: EndpointAnswer = await contacts.get_customer_contacts_by_inn(customer_inn=customer_inn)
+
+    if answer.status != "success":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=parse_message(answer.message)
+        )
+
+    return answer
 
 
-@router.post("/", response_model=CustomerContactOutSchema, status_code=status.HTTP_202_ACCEPTED)
+@router.post("/", response_model=EndpointAnswer, status_code=status.HTTP_202_ACCEPTED)
 async def create_customer_contact(
         contact: CustomerContactInSchema,
         contacts: CustomerContactRepository = Depends(get_customer_contact_repository)):
-    return await contacts.create_customer_contact(contact=contact)
+    answer: EndpointAnswer = await contacts.create_customer_contact(contact=contact)
+
+    if answer.status != "success":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=parse_message(answer.message)
+        )
+
+    return answer
 
 
-@router.put("/", response_model=CustomerContactOutSchema, status_code=status.HTTP_202_ACCEPTED)
+@router.put("/", response_model=EndpointAnswer, status_code=status.HTTP_202_ACCEPTED)
 async def update_customer_contact(
         contact_id: int,
         customer_inn: str,
         contact: CustomerContactInSchema,
         contacts: CustomerContactRepository = Depends(get_customer_contact_repository)):
-    return await contacts.update_contact(contact_id=contact_id, customer_inn=customer_inn, contact=contact)
+    answer: EndpointAnswer = await contacts.update_contact(contact_id=contact_id, customer_inn=customer_inn,
+                                                           contact=contact)
+
+    if answer.status != "success":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=parse_message(answer.message)
+        )
+
+    return answer
 
 
-@router.delete("/", response_model=dict, status_code=status.HTTP_202_ACCEPTED)
+@router.delete("/", response_model=EndpointAnswer, status_code=status.HTTP_202_ACCEPTED)
 async def delete_customer_contact_by_id(
         contact_id: int,
         contacts: CustomerContactRepository = Depends(get_customer_contact_repository)):
-    return await contacts.delete_customer_contact_by_id(contact_id=contact_id)
+    answer: EndpointAnswer = await contacts.delete_customer_contact_by_id(contact_id=contact_id)
+
+    if answer.status != "success":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=parse_message(answer.message)
+        )
+
+    return answer
--- project/app/repositories/customer_contact.py
+++ project/app/repositories/customer_contact.py
@@ -1,5 +1,3 @@
-from typing import List
-from fastapi import HTTPException, status
 from datetime import datetime
 from sqlalchemy import and_
 
@@ -7,29 +5,34 @@ from project.app.helper.log import logger
 from project.app.repositories.base_repository import BaseRepository
 from project.app.schema.customer_contact import CustomerContactInSchema, CustomerContactOutSchema
 from project.app.db.tables.customer import customer_contact
+from project.app.helper.endpoint_answer import EndpointAnswer
 
 
 class CustomerContactRepository(BaseRepository):
 
     @logger.catch
-    async def get_customer_contacts_by_inn(self, customer_inn: str) -> List[CustomerContactOutSchema]:
+    async def get_customer_contacts_by_inn(self, customer_inn: str) -> EndpointAnswer:
         query = customer_contact.select().where(customer_contact.c.inn_customer == customer_inn)
-        contact_list = await self.database.fetch_all(query)
+        try:
+            contact_list = await self.database.fetch_all(query)
+            if contact_list:
+                contact_list_obj = []
+                for c in contact_list:
+                    contact_list_obj.append(CustomerContactOutSchema.parse_obj(c))
+                message = f"Contact with inn {customer_inn} was found."
+                result = EndpointAnswer(status="success", message=message, result=contact_list_obj)
+                logger.info(message)
+            else:
+                message = f"Contact with inn {customer_inn} was not found."
+                result = EndpointAnswer(status="success", message=message)
+        except Exception as e:
+            result = EndpointAnswer(status="fail", message=str(e))
+            logger.error(str(e))
 
-        if not contact_list:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"There is no contacts for customer with inn:'{customer_inn}'."
-            )
-
-        contact_list_obj = []
-        for c in contact_list:
-            contact_list_obj.append(CustomerContactOutSchema.parse_obj(c))
-
-        return contact_list_obj
+        return result
 
     @logger.catch
-    async def create_customer_contact(self, contact: CustomerContactInSchema) -> CustomerContactOutSchema:
+    async def create_customer_contact(self, contact: CustomerContactInSchema) -> EndpointAnswer:
         query = customer_contact.insert().values(
             inn_customer=contact.inn_customer,
             name=contact.name,
@@ -41,28 +44,31 @@ class CustomerContactRepository(BaseRepository):
             created_at=datetime.utcnow(),
             updated_at=datetime.utcnow()
         )
-        new_id = await self.database.execute(query)
-        query = customer_contact.select().where(customer_contact.c.id == new_id)
-        new_contact = await self.database.fetch_one(query)
+        try:
+            new_id = await self.database.execute(query)
+            query = customer_contact.select().where(customer_contact.c.id == new_id)
+            new_contact = await self.database.fetch_one(query)
+            if not new_contact:
+                message: str = "Contact can't be inserted. Please try it again."
+                logger.info(message)
+                result = EndpointAnswer(status="success", message=message)
+            else:
+                message: str = \
+                    f"New contact with id:'{new_id}' for customer with inn:'{contact.inn_customer}' was created."
+                logger.info(message)
+                result = EndpointAnswer(status="success", message=message, result=new_contact)
+        except Exception as e:
+            result = EndpointAnswer(status="fail", message=str(e))
+            logger.error(str(e))
 
-        if not new_contact:
-            message: str = "Contact can't be inserted. Please try it again."
-            logger.error(message)
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=message
-            )
-
-        logger.info(f"New contact with id:'{new_id}' for customer with inn:'{contact.inn_customer}' was created.")
-
-        return CustomerContactOutSchema.parse_obj(new_contact)
+        return result
 
     @logger.catch
     async def update_contact(
             self,
             contact_id: int,
             customer_inn: str,
-            contact: CustomerContactInSchema) -> CustomerContactOutSchema:
+            contact: CustomerContactInSchema) -> EndpointAnswer:
         query = customer_contact. \
             update(). \
             where(and_(customer_contact.c.id == contact_id, customer_contact.c.inn_customer == customer_inn)). \
@@ -76,24 +82,33 @@ class CustomerContactRepository(BaseRepository):
                 email=contact.email,
                 updated_at=datetime.utcnow()
             )
+        try:
+            await self.database.execute(query)
 
-        await self.database.execute(query)
+            query = customer_contact.select().where(
+                and_(
+                    customer_contact.c.inn_customer == customer_inn,
+                    customer_contact.c.id == contact_id)
+            )
+            updated_contact = await self.database.fetch_one(query)
+            message = f"Contact with id:{contact_id} for customer with inn:'{customer_inn}' was updated."
+            result = EndpointAnswer(status="success", message=message, result=updated_contact)
+            logger.info(message)
+        except Exception as e:
+            result = EndpointAnswer(status="fail", message=str(e))
+            logger.error(str(e))
 
-        query = customer_contact.select().where(
-            and_(
-                customer_contact.c.inn_customer == customer_inn,
-                customer_contact.c.id == contact_id)
-        )
-        updated_contact = await self.database.fetch_one(query)
-
-        logger.info(f"Contact with id:{contact_id} for customer with inn:'{customer_inn}' was updated.")
-        return CustomerContactOutSchema.parse_obj(updated_contact)
+        return result
 
     @logger.catch
-    async def delete_customer_contact_by_id(self, contact_id: int) -> dict:
+    async def delete_customer_contact_by_id(self, contact_id: int) -> EndpointAnswer:
         query = customer_contact.delete().where(customer_contact.c.id == contact_id)
-        await self.database.execute(query)
-
-        message = f"Contact with id:'{contact_id}' was deleted."
-        logger.info(message)
-        return {"message": message}
+        try:
+            await self.database.execute(query)
+            message = f"Contact with id:'{contact_id}' was deleted."
+            result = EndpointAnswer(status="success", message=message)
+            logger.info(message)
+        except Exception as e:
+            result = EndpointAnswer(status="fail", message=str(e))
+            logger.error(str(e))
+        return result
