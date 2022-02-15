b527b14c7f60f1263de656c0db1ab4fe622f49a5
Maxim <maxim.bienemann@gmail.com>
2022-02-15 08:00:55 +0100

    Change hash_password -> password_hash

--- project/app/core/security.py
+++ project/app/core/security.py
@@ -7,5 +7,5 @@ def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def verify_password(password: str, hash_password: str) -> bool:
-    return pwd_context.verify(password, hash_password)
+def verify_password(password: str, password_hash: str) -> bool:
+    return pwd_context.verify(password, password_hash)
