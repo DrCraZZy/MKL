db09f23889c250edeb603726104404a2d89611ff
Maxim <maxim.bienemann@gmail.com>
2022-02-15 08:19:08 +0100

    Changes for create token

--- project/app/core/security.py
+++ project/app/core/security.py
@@ -1,4 +1,8 @@
+import datetime
 from passlib.context import CryptContext
+from jose import jwt
+
+from project.app.core.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
 
 pwd_context = CryptContext(schemas=["bcrypt"])
 
@@ -9,3 +13,17 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, password_hash: str) -> bool:
     return pwd_context.verify(password, password_hash)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    to_encode.update({"exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_access_token(token: str):
+    try:
+        encoded_jwt = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    except jwt.JWTError:
+        return None
+    return encoded_jwt
